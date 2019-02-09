b6be6da882ef3c8efb38827e34eb88abd22342f6
John Alden <jzalden96@gmail.com>
2019-02-09 00:59:18 -0700

    Commit all the relevant files.

---
+++ spyce/py_wrapper.cpp
@@ -0,0 +1,36 @@
+#include <boost/python.hpp>
+
+#include "spyce_exceptions.hpp"
+#include "spyce.hpp"
+
+
+//credit: https://stackoverflow.com/questions/2261858/boostpython-export-custom-exception
+namespace py = boost::python;
+template <class E, class... Policies, class... Args>
+py::class_<E, Policies...> exception_(Args&&... args) {
+    py::class_<E, Policies...> cls(std::forward<Args>(args)...);
+    py::register_exception_translator<E>([ptr=cls.ptr()](E const& e){
+        PyErr_SetObject(ptr, py::object(e).ptr());
+    });
+    return cls;
+}
+
+BOOST_PYTHON_MODULE(spyce) {
+    using namespace boost::python;
+
+    exception_<FileNotFoundException>("FileNotFoundException")
+        .add_property("what", &FileNotFoundException::what);
+
+    class_<spyce>("spyce")
+        .add_property("main_file",&spyce::_get_file, &spyce::_set_file)
+        .def("add_kernel", &spyce::add_kernel)
+        .def("remove_kernel", &spyce::remove_kernel);
+
+    class_<Frame>("Frame")
+        .def_readwrite("x",  &Frame::x)
+        .def_readwrite("y",  &Frame::y)
+        .def_readwrite("z",  &Frame::z)
+        .def_readwrite("dx", &Frame::dx)
+        .def_readwrite("dy", &Frame::dy)
+        .def_readwrite("dz", &Frame::dz);
+}
