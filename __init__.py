571a821a701a2ddbf235776741d9ac2152ad0971
dlahtine <dlahtine@asu.edu>
2019-02-11 19:43:30 -0700

    Created endpoints for sun, moon, spacecraft position and orientation.
    Created endpoints for accessing and replacing SPK file.
    
    implemented functionality for serving webpages.

--- __init__.py
+++ __init__.py
@@ -1,21 +1,54 @@
-from flask import Flask
+from flask import Flask, render_template, request, Response, send_from_directory
+#from spyce import spyce
+import os
 
-app = Flask(__name__)
+
+template_path = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + "/web")
+#print (template_path)
+app = Flask(__name__, template_folder=template_path)
 
 @app.route('/')
-def home_page():
+def root():
     return "This is the root endpoint for this server"
 
-@app.route('/js/index.js')
-def get_index():
+@app.route('/web/index.html', methods=['GET'])
+def home_page():
+    return render_template('index.html')
+
+@app.route('/web/js/<path:path>', methods=['GET'])
+def get_javascript(path):
+    return send_from_directory('web/js', path)
+
+@app.route('/spacecraft/pos', methods=['GET'])
+def get_spacecraft_pos():
     return "TODO"
 
-@app.route('/js/OrbitControls.js')
-def get_orbit_controls():
+@app.route('/spacecraft/orientation', methods=['GET'])
+def get_spacecraft_orientation():
     return "TODO"
 
-@app.route('/js/three.js')
-def get_three():
+@app.route('/moon/pos', methods=['GET'])
+def get_moon_pos():
+    return "TODO"
+
+@app.route('/moon/orientation', methods=['GET'])
+def get_moon_orientation():
+    return "TODO"
+
+@app.route('/sun/pos', methods=['GET'])
+def get_sun_pos():
+    return "TODO"
+
+@app.route('/sun/orientation', methods=['GET'])
+def get_sun_orientation():
+    return "TODO"
+
+@app.route('/data/trajectory', methods=['GET'])
+def get_trajectory_file():
+    return "TODO"
+
+@app.route('/data/trajectory', methods=['POST', 'PUT'])
+def change_trajectory_file():
     return "TODO"
 
 if __name__ == '__main__':
