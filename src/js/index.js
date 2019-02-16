2ec034b45c0b34faafbe4cd7437ff3d6cb909fc2
Trevor Forrey <trevor4e@gmail.com>
2019-02-16 12:49:44 -0700

    US-21 Task-47 Solar Flare Works....ish
    But the light is at the opposite side of the solar flare

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,25 @@
 import * as THREE from './three'
 import OrbitControls from './OrbitControls'
+import LesnFlare from './LensFlare'
 import Earth from './models/earth'
 import Moon from './models/moon'
 import Sun from './models/sun'
+import solarFlare from '../lensflare0.png'
+import solarBubble from '../lensflare3.png'
 
 // Constants
 const sunScale = 5;
 const earthScale = 4;
 const moonScale = 1;
 const moonOrbitRadius = 50; //Real Life: Radius is 100x that of the size of the moon
-const earthOrbitRaius = 930;
+const earthOrbitRaius = 50;
 
+// Function-like promise loader
+const loadTexture = (path, loader, onProgress) => { 
+    return new Promise((resolve, reject) => {
+        loader.load(path, resolve, onProgress, reject);
+    });
+}
 
 // Create scene and camera
 let scene = new THREE.Scene();
@@ -36,11 +45,42 @@ window.addEventListener('resize', function() {
     camera.updateProjectionMatrix();
 });
 
+var textureFlare0;
+var textureFlare3;
+let solarFlareLight;
 
-//This lighting makes the Sun glow and removes shadow from the sun
-//Ambient and Directional light do not look as good as HemiLight
-let hemiLight = new THREE.HemisphereLight( 0xf2c559, 0xffffff, 1.25 );
-scene.add(hemiLight);
+loadTexture(solarFlare, new THREE.TextureLoader()).then((flareTexture) => {
+    textureFlare0 = flareTexture;
+    loadTexture(solarBubble, new THREE.TextureLoader());
+}).then((bubbleFlareTexture) => {
+    textureFlare3 = bubbleFlareTexture;
+}).then(() => {
+    solarFlareLight = addLight( 0.995, 0.5, 0.9, 0, 0, 0 );
+}).catch((err) => {
+    console.error(err);
+});
+
+// https://threejs.org/examples/#webgl_lensflares
+function addLight( h, s, l, x, y, z ) {
+
+    var light = new THREE.PointLight( 0xffffff, 1.5, 2000 );
+    light.color.setHSL( h, s, l );
+    light.position.set( x, y, z );
+    scene.add( light );
+
+    console.log(textureFlare0);
+    console.log(textureFlare3);
+
+    var lensflare = new THREE.Lensflare();
+    lensflare.addElement( new THREE.LensflareElement( textureFlare0, 100, 1, light.color ) );
+    lensflare.addElement( new THREE.LensflareElement( textureFlare3, 60, 0.6 ) );
+    lensflare.addElement( new THREE.LensflareElement( textureFlare3, 70, 0.7 ) );
+    lensflare.addElement( new THREE.LensflareElement( textureFlare3, 120, 0.9 ) );
+    lensflare.addElement( new THREE.LensflareElement( textureFlare3, 70, 1 ) );
+    light.add( lensflare );
+
+    return light;
+}
 
 // Add X, Y, Z axis helper (axes are colored in scene)
 let axesHelper = new THREE.AxesHelper( 5 );
@@ -53,8 +93,11 @@ let axis = new THREE.Vector3(0,0.4101524,0).normalize();
 const update = () => {
     let date = Date.now() * 0.00001;
 
-    sun.position.x = earth.position.x + Math.cos(date) * earthOrbitRaius;
-    sun.position.z = earth.position.z + Math.sin(date) * earthOrbitRaius;
+    // sun.position.x = earth.position.x + Math.cos(date) * earthOrbitRaius;
+    // sun.position.z = earth.position.z + Math.sin(date) * earthOrbitRaius;
+
+    solarFlareLight.position.x = earth.position.x + Math.cos(date * 10) * earthOrbitRaius;
+    solarFlareLight.position.z = earth.position.z + Math.sin(date * 10) * earthOrbitRaius;
 
     moon.position.x = earth.position.x + Math.cos(date * 2) * moonOrbitRadius;
     moon.position.z = earth.position.z + Math.sin(date * 2) * moonOrbitRadius;
@@ -92,9 +135,9 @@ earth.load().then((earthMesh) => {
     scene.add(moon);
     return sun.load();
 }).then((sunMesh) => {
-    sun = sunMesh;
-    sun.scale.set(sunScale,sunScale,sunScale);
-    scene.add(sun);
+    // sun = sunMesh;
+    // sun.scale.set(sunScale,sunScale,sunScale);
+    // scene.add(sun);
 }).then(() => {
     animate();
 });;
