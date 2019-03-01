80ea470d94e53501e1e302ffa53121d1ff86b47e
dlahtine <dlahtine@asu.edu>
2019-03-01 13:29:24 -0700

    Should load spacecraft from config properly.

--- FlaskServer.py
+++ FlaskServer.py
@@ -6,28 +6,27 @@ CURRENT_PATH = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 template_path = os.path.abspath(CURRENT_PATH + "/dist")
 app = Flask(__name__, template_folder=template_path, static_url_path='', static_folder=None)
 spy = spyce.spyce()
-BSP_FILENAME = "spacecraft.bsp"
-TRAJECTORY_FOLDER = CURRENT_PATH + "/data/trajectory/"
+main_filename = ""
+TRAJECTORY_FOLDER = CURRENT_PATH + "/config/spyce_files/trajectory/"
 spy_loaded = False
 
+main_subject = None
+
+#being replaced by john's idtoname
+"""
 astral_names = {
     10: "SUN",
     399: "EARTH"
 }
-
-#loading initial file for spy
-#snippet derived from https://stackoverflow.com/questions/1724693/find-a-file-in-python
-def load_spacecraft_bsp():
-    for root, dirs, files in os.walk(TRAJECTORY_FOLDER):
-        if "spacecraft.bsp" in files:
-                spy.main_file = os.path.join(root, BSP_FILENAME)
-                spy_loaded = True
+"""
 
 def load_config():
-    with open('config.json') as conf_file:
+    with open('config/config.json') as conf_file:
         conf_data = json.load(conf_file)
-        for sc in conf_data['spacecraft']:
-            astral_names[sc["id"]] = sc["name"]
+        spy.main_file = CURRENT_PATH + "/config/spyce_files/trajectory/" + conf_data['main_file']
+        main_subject = conf_data['main_subject']
+        for kern in conf_data['kernels']:
+            spy.add_kernel("config/spyce_files/kernels/" + kern)
 
 @app.route('/')
 def root():
@@ -42,65 +41,20 @@ def get_spacecraft_pos():
 def get_all_objects():
     jsonResponse = []
     time = request.args.get('time')
-    for id in spy.getObjects():
-        name = astral_names[id]
+    time = float(time)
+    for id in spy.get_objects():
         frame = spy.get_frame_data(id, 399, time)
         frame_as_dict = frame_to_dict(frame)
         celestialObj = {}
         celestialObj['id'] = id
-        celestialObj['name'] = name
+        #TODO: add john's idtoname
         celestialObj['frame'] = frame_as_dict
         jsonResponse.append(celestialObj)
     return jsonify(jsonResponse)
 
-
-#code derived from http://flask.pocoo.org/docs/1.0/patterns/fileuploads/
-#Apparently, sponsor will not be using our server to upload files.
-"""
-@app.route('/data/trajectory', methods=['GET', 'POST'])
-def change_trajectory_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            print ("[WARN]: No file recieved.")
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            print ("[WARN]: No file selected.")
-            return redirect(request.url)
-        if file and file.filename[-4:] == ".bsp":
-            file.save(os.path.join(TRAJECTORY_FOLDER, BSP_FILENAME))
-            #TODO: replace with actual webpages
-            try:
-                load_spacecraft_bsp()
-                return "File change successful!"
-            except:
-                return "Unable to use BSP file"
-        print ("[WARN]: improper file format")
-
-    #TODO: replace with actual webpage.
-    return '''
-        <!doctype html>
-        <title>Upload new File</title>
-        <h1>Upload new File</h1>
-        <form method=post enctype=multipart/form-data>
-          <input type=file name=file>
-          <input type=submit value=Upload>
-        </form>
-        '''
-"""
-
 @app.route('/<path:filename>', methods=['GET'])
 def get_file(filename):
     return send_from_directory('dist', filename)
-
-if __name__ == '__main__':
-    print(app.url_map)
-    load_config()
-    app.run(debug=True)
-    try:
-        load_spacecraft_bsp()
-    except:
-        print ("[WARN]: Unable to load BSP file")
 
 def frame_to_dict(frame):
     frameDict = {}
@@ -111,3 +65,13 @@ def frame_to_dict(frame):
     frameDict['dy'] = frame.dy
     frameDict['dz'] = frame.dz
     return frameDict
+
+if __name__ == '__main__':
+    print(app.url_map)
+    try:
+        load_config()
+    except:
+        print ("[ERROR]: Unable to load config")
+    app.run(debug=True)
+
+
