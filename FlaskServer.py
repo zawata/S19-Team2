ea7b632e329e1251f543652e9a59faec0f3b53b6
dlahtine <dlahtine@asu.edu>
2019-02-28 18:35:12 -0700

    Should load spacecraft from config properly.

--- FlaskServer.py
+++ FlaskServer.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, Response, send_from_directory, redirect, url_for
+from flask import Flask, render_template, request, Response, send_from_directory, redirect, url_for, jsonify
 from spyce import spyce
 import os, json
 
@@ -18,20 +18,40 @@ def load_spacecraft_bsp():
                 spy.main_file = os.path.join(root, BSP_FILENAME)
                 spy_loaded = True
 
+def load_config():
+    with open('config.json') as conf_file:
+        conf_data = json.load(conf_file)
+        for sc in conf_data['spacecraft']:
+            astral_names[sc["id"]] = sc["name"]
+
 @app.route('/')
 def root():
     return redirect("/index.html")
 
 
-@app.route('/spacecraft/pos', methods=['GET'])
+@app.route('/api/spacecraft/pos', methods=['GET'])
 def get_spacecraft_pos():
     return "TODO"
 
 @app.route('/api/all_objects', methods=['GET'])
 def get_all_objects():
-    return "TODO"
+    jsonResponse = []
+    time = request.args.get('time')
+    for id in spy.getObjects():
+        name = astral_names[id]
+        frame = spy.get_frame_data(id, 399, time)
+        frame_as_dict = frame_to_dict(frame)
+        celestialObj = {}
+        celestialObj['id'] = id
+        celestialObj['name'] = name
+        celestialObj['frame'] = frame_as_dict
+        jsonResponse.append(celestialObj)
+    return jsonify(jsonResponse)
+
 
 #code derived from http://flask.pocoo.org/docs/1.0/patterns/fileuploads/
+#Apparently, sponsor will not be using our server to upload files.
+"""
 @app.route('/data/trajectory', methods=['GET', 'POST'])
 def change_trajectory_file():
     if request.method == 'POST':
@@ -62,11 +82,14 @@ def change_trajectory_file():
           <input type=submit value=Upload>
         </form>
         '''
+"""
 
 @app.route('/<path:filename>', methods=['GET'])
 def get_file(filename):
     return send_from_directory('dist', filename)
+
 if __name__ == '__main__':
+    load_config()
     port = os.getenv('PORT', 5000)
     host = '0.0.0.0'
 
@@ -76,12 +99,12 @@ if __name__ == '__main__':
     except:
         print ("[WARN]: Unable to load BSP file")
 
-def frame_to_json(frame):
-    jsonData = {}
-    jsonData['x'] = frame.x
-    jsonData['y'] = frame.y
-    jsonData['z'] = frame.z
-    jsonData['dx'] = frame.dx
-    jsonData['dy'] = frame.dy
-    jsonData['dz'] = frame.dz
-    return json.dumps(jsonData)
+def frame_to_dict(frame):
+    frameDict = {}
+    frameDict['x'] = frame.x
+    frameDict['y'] = frame.y
+    frameDict['z'] = frame.z
+    frameDict['dx'] = frame.dx
+    frameDict['dy'] = frame.dy
+    frameDict['dz'] = frame.dz
+    return frameDict
