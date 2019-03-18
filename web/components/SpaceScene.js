8cc71c12461149534d29dc1b49b42a05157a51c3
Trevor Forrey <trevor4e@gmail.com>
2019-03-17 23:20:02 -0700

    Factored out build scene and solar flare

--- web/components/SpaceScene.js
+++ web/components/SpaceScene.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import * as THREE from '../js/three';
 import LensFlare from '../js/LensFlare'
 import OrbitControls from '../js/OrbitControls';
-import solarFlare from '../lensflare0.png';
-import solarBubble from '../lensflare3.png';
 import Earth from '../js/models/earth'
 import Moon from '../js/models/moon'
 import Sun from '../js/models/sun'
+import { buildScene, makeTextureFlare, makePointLight, makeLensflare } from './sceneHelper';
 
 const sunScale = 5;
 const earthScale = 4;
@@ -24,64 +23,16 @@ const loadTexture = (path, loader, onProgress) => {
 export default class SpaceScene extends Component {
 
   componentDidMount() {
-    const width = this.mount.clientWidth;
-    const height = this.mount.clientHeight;
+    
+    let { scene, camera, controls, renderer } = buildScene();
 
-    let scene = new THREE.Scene();
-
-    //The scenes share the same camera, controls, renderer defined below
-    let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 10; // set camera away from origin
-    camera.position.x = 10;
-    camera.position.y = 0;
-
-    // Add mouse controls
-    const controls = new OrbitControls(camera);
-
-    // Create renderer
-    let renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    //Code to adapt to resizing of windows- fit to window size
-    window.addEventListener('resize', function() {
-      let widthWindow = window.innerWidth;
-      let heightWindow = window.innerHeight;
-      renderer.setSize(widthWindow, heightWindow);
-      camera.aspect = widthWindow / heightWindow;
-      camera.updateProjectionMatrix();
+    let pointLight;
+    makeTextureFlare().then(textureFlare => {
+      pointLight = makePointLight(0.995, 0.5, 0.9, 0, 0, 0);
+      let lensFlare = makeLensflare(textureFlare, pointLight);
+      scene.add(pointLight);
+      pointLight.add(lensFlare);
     });
-
-    //The following textures and flares are for the Sun in the first scene
-    let textureFlare0;
-    let textureFlare3;
-    let solarFlareLight;
-
-    loadTexture(solarFlare, new THREE.TextureLoader()).then((flareTexture) => {
-      textureFlare0 = flareTexture;
-      loadTexture(solarBubble, new THREE.TextureLoader());
-    }).then((bubbleFlareTexture) => {
-      textureFlare3 = bubbleFlareTexture;
-    }).then(() => {
-      solarFlareLight = addLight(0.995, 0.5, 0.9, 0, 0, 0);
-    }).catch((err) => {
-      console.error(err);
-    });
-
-    // https://threejs.org/examples/#webgl_lensflares
-    function addLight(h, s, l, x, y, z) {
-
-      let light = new THREE.PointLight(0xffffff, 1.5, 2000);
-      light.color.setHSL(h, s, l);
-      light.position.set(x, y, z);
-      scene.add(light);
-
-      let lensflare = new THREE.Lensflare();
-      lensflare.addElement(new THREE.LensflareElement(textureFlare0, 100, 0, light.color));
-      light.add(lensflare);
-
-      return light;
-    }
 
     // Add X, Y, Z axis helper (axes are colored in scene)
     let axesHelper = new THREE.AxesHelper(5);
@@ -90,14 +41,13 @@ export default class SpaceScene extends Component {
     // Create axis of rotation
     let axis = new THREE.Vector3(0, 0.4101524, 0).normalize();
 
-
     //Scene 1 spheres
     // Declaring update, render, and animation function: rotations for the first scene
     const update = () => {
       let date = Date.now() * 0.00001;
 
-      solarFlareLight.position.x = earth.position.x + Math.sin(date) * earthOrbitRadius;
-      solarFlareLight.position.z = earth.position.z + Math.cos(date) * earthOrbitRadius;
+      pointLight.position.x = earth.position.x + Math.sin(date) * earthOrbitRadius;
+      pointLight.position.z = earth.position.z + Math.cos(date) * earthOrbitRadius;
 
       moon.position.x = earth.position.x + Math.sin(date * 3) * moonOrbitRadius;
       moon.position.z = earth.position.z + Math.cos(date * 3) * moonOrbitRadius;
@@ -105,10 +55,9 @@ export default class SpaceScene extends Component {
       earth.rotateOnAxis(axis, 0.0009);
       moon.rotateOnAxis(axis, 0.001); //moon's rotation on its axis
 
-      controls.target.set(moon.position.x, moon.position.y, moon.position.z);
+      // controls.target.set(moon.position.x, moon.position.y, moon.position.z);
     };
 
-    let sun = new Sun(sunScale);
     let earth = new Earth(0.5, earthScale);
     let moon = new Moon(0.1, moonScale);
 
@@ -124,11 +73,6 @@ export default class SpaceScene extends Component {
     }).then((moonMesh) => {
       moon = moonMesh;
       scene.add(moon);
-      return sun.load();
-    }).then((sunMesh) => {
-      // sun = sunMesh;
-      // sun.scale.set(sunScale,sunScale,sunScale);
-      // spaceScene.add(sun);
     }).then(() => {
       animate();
     });;
---
+++ web/components/sceneHelper.js
@@ -0,0 +1,63 @@
+import * as THREE from '../js/three';
+import OrbitControls from '../js/OrbitControls';
+import solarFlare from '../lensflare0.png';
+import solarBubble from '../lensflare3.png';
+
+export function buildScene() {
+  let scene = new THREE.Scene();
+
+  //The scenes share the same camera, controls, renderer defined below
+  let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 10; // set camera away from origin
+  camera.position.x = 10;
+  camera.position.y = 0;
+
+  // Add mouse controls
+  const controls = new OrbitControls(camera);
+
+  // Create renderer
+  let renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  //Code to adapt to resizing of windows- fit to window size
+  window.addEventListener('resize', function() {
+    let widthWindow = window.innerWidth;
+    let heightWindow = window.innerHeight;
+    renderer.setSize(widthWindow, heightWindow);
+    camera.aspect = widthWindow / heightWindow;
+    camera.updateProjectionMatrix();
+  });
+
+  return {
+    scene,
+    camera,
+    controls,
+    renderer
+  }
+}
+
+export function makeTextureFlare() {
+  return loadTexture(solarFlare, new THREE.TextureLoader())
+}
+
+export function makePointLight(h, s, l, x, y, z) {
+  let light = new THREE.PointLight(0xffffff, 1.5, 2000);
+  light.color.setHSL(h, s, l);
+  light.position.set(x, y, z);
+
+  return light;
+}
+
+export function makeLensflare(textureFlare, light) {
+  let lensflare = new THREE.Lensflare();
+  lensflare.addElement(new THREE.LensflareElement(textureFlare, 100, 0, light.color));
+  return lensflare;
+}
+
+// Function-like promise loader
+const loadTexture = (path, loader, onProgress) => {
+  return new Promise((resolve, reject) => {
+    loader.load(path, resolve, onProgress, reject);
+  });
+}
