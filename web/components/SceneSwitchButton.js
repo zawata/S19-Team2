aee57abdb11eed37885c74f9c145eb9e695736f2
Trevor Forrey <trevor4e@gmail.com>
2019-03-22 15:28:13 -0700

    React Conversion: cleaned up scene switch button styling

--- web/components/SceneSwitchButton.js
+++ web/components/SceneSwitchButton.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
 export default class SceneSwitchButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+
+  handleToggle() {
+    console.log('I was clicked!');
+    event.target.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i> loading...';
+  }
+
   render() {
     return(
       <div className="changeSceneButtonID">
-        <button type="button" className="btn btn-secondary">
-          <i className="fas fa-rocket">Fly to Moon</i> 
+        <button onClick={this.handleToggle} className="btn btn-secondary">
+          <i className="fas fa-rocket"></i>
+          <span className="button-text">View the Moon</span>
         </button>
       </div>
     )
