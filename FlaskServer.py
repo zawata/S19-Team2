11680f1a8a0850cd1afaf440f43e643e41c641f5
dlahtine <dlahtine@asu.edu>
2019-03-22 21:28:55 -0700

    endpoint created for getting info on single object

--- FlaskServer.py
+++ FlaskServer.py
@@ -11,6 +11,7 @@ TRAJECTORY_FOLDER = CURRENT_PATH + "/config/spyce_files/trajectory/"
 kernels = []
 main_subject = None
 main_subject_name = ""
+EARTH = 399
 
 def load_config():
     with open('config/config.json') as conf_file:
@@ -40,41 +41,22 @@ def get_main_id():
     jsonResponse['name'] = main_subject_name
     return jsonify(jsonResponse)
 
+@app.route('/api/get_object', methods=['GET'])
+def get_object():
+    time = request.args.get('time')
+    observer = request.args.get('observer')
+    if observer == None:
+        observer = EARTH
+    object_id = request.args.get('object_id')
+    return jsonify(get_objects(time=time, observer=observer, object_id=object_id))
+
 @app.route('/api/all_objects', methods=['GET'])
 def get_all_objects():
-    jsonResponse = []
     time = request.args.get('time')
-    frame_data_requested = time != None
-    id = None
-    if (frame_data_requested):
-        time = float(time)
-    for k in kernels:
-        spy.main_file = k
-        try:
-            for id in spy.get_objects():
-                celestialObj = {}
-                celestialObj['id'] = id
-                if (frame_data_requested):
-                    frame = spy.get_frame_data(id, 399, time)
-                    frame_as_dict = frame_to_dict(frame)
-                    celestialObj['frame'] = frame_as_dict
-                name = ""
-                if id == main_subject:
-                    name = main_subject_name
-                else:
-                    try:
-                        name = spyce.id_to_str(id)
-                    except:
-                        print("[ERROR]: NAIF not found")
-                celestialObj['name'] = name
-                jsonResponse.append(celestialObj)
-        except spyce.InternalError:
-            #thrown when kernel does not have objects, like leapseconds
-            pass
-        except spyce.InsufficientDataError:
-            #An object does not have frame data for this instant
-            print("[WARN]: object %s does not have data for %s" % (id, time))
-    return jsonify(jsonResponse)
+    observer = request.args.get('observer')
+    if observer == None:
+        observer = EARTH
+    return jsonify(get_objects(time=time, observer=observer))
 
 @app.route('/<path:filename>', methods=['GET'])
 def get_file(filename):
@@ -89,6 +71,49 @@ def frame_to_dict(frame):
     frameDict['dy'] = frame.dy
     frameDict['dz'] = frame.dz
     return frameDict
+
+def get_objects(**kwargs):
+    time = kwargs.get('time', None)
+    object_id = kwargs.get('object_id', None)
+    observer = kwargs.get('observer', EARTH)
+
+    ret = []
+    frame_data_requested = time != None
+    all_objects_requested = object_id == None
+
+    for k in kernels:
+        spy.main_file = k
+        try:
+            for id in spy.get_objects():
+                if all_objects_requested and id != object_id:
+                    continue
+                celestialObj = {}
+                celestialObj['id'] = id
+                if (frame_data_requested):
+                    frame = spy.get_frame_data(id, observer, time)
+                    frame_as_dict = frame_to_dict(frame)
+                    celestialObj['frame'] = frame_as_dict
+                name = ""
+                if id == main_subject:
+                    name = main_subject_name
+                else:
+                    try:
+                        name = spyce.id_to_str(id)
+                    except:
+                        print("[ERROR]: NAIF not found: ", id)
+                celestialObj['name'] = name
+                if all_objects_requested:
+                    ret.append(celestialObj)
+                else:
+                    return celestialObj
+        except spyce.InternalError:
+            #thrown when kernel does not have objects, like leapseconds
+            print("[ERROR]: spyce does not have enough info to properly calculate request.")
+            pass
+        except spyce.InsufficientDataError:
+            #An object does not have frame data for this instant
+            print("[WARN]: object %s does not have data for %s" % (id, time))
+    return ret
 
 if __name__ == '__main__':
     try:
