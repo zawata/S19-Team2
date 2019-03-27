09ef786a6db331c68034816ea0e6e8ec7854bf41
Kritika Bhat <kbhat3@asu.edu>
2019-03-27 16:06:22 -0700

    Dummy Slider created. Div positioned on right below button temprorary

---
+++ web/components/SpeedSliderContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import SpeedSlider from './SpeedSlider';
+
+export default class SpeedSliderContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className="speed-slider-container">
+        <div>
+          <SpeedSlider/>
+        </div>
+      </div>
+    )
+  }
+}
