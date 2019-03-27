2c1620564705ae56a5cae46cae79183b1bdcf525
Kritika Bhat <kbhat3@asu.edu>
2019-03-27 16:06:22 -0700

    Dummy Slider created. Div positioned on right below button temprorary

---
+++ web/components/SpeedSlider.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import { render } from "react-dom";
+import { Slider, Rail, Handles, Tracks } from 'react-compound-slider'
+import { SliderRail, Handle, Track } from "./sliderComponents"; // example render components - source below
+
+const sliderStyle = {
+  position: 'relative',
+  width: '100%',
+}
+
+const domain = [100, 500]
+const defaultValues = [150]
+
+export default class SpeedSlider extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      values: defaultValues.slice(),
+      update: defaultValues.slice(),
+    };
+  }
+
+  onUpdate = update => {
+    this.setState({ update })
+  }
+
+  onChange = values => {
+    this.setState({ values })
+  }
+
+  render() {
+    const {
+      state: { values, update },
+    } = this
+
+    return (
+      <div className="speed-slider-core-container" style={{ height: 120, width: '100%' }}>
+        <Slider
+          mode={1}
+          step={1}
+          domain={domain}
+          rootStyle={sliderStyle}
+          onUpdate={this.onUpdate}
+          onChange={this.onChange}
+          values={values}
+        >
+          <Rail>
+            {({ getRailProps }) => <SliderRail getRailProps={getRailProps} />}
+          </Rail>
+          <Handles>
+            {({ handles, getHandleProps }) => (
+              <div className="slider-handles">
+                {handles.map(handle => (
+                  <Handle
+                    key={handle.id}
+                    handle={handle}
+                    domain={domain}
+                    getHandleProps={getHandleProps}
+                  />
+                ))}
+              </div>
+            )}
+          </Handles>
+          <Tracks right={false}>
+            {({ tracks, getTrackProps }) => (
+              <div className="slider-tracks">
+                {tracks.map(({ id, source, target }) => (
+                  <Track
+                    key={id}
+                    source={source}
+                    target={target}
+                    getTrackProps={getTrackProps}
+                  />
+                ))}
+              </div>
+            )}
+          </Tracks>
+        </Slider>
+      </div>
+    )
+  }
+}
