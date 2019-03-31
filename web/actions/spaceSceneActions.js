92c288df0c9939d049811c0ad5bb4fc101789ec7
Trevor Forrey <trevor4e@gmail.com>
2019-03-30 19:32:29 -0700

    Connected slider component to redux store
    Now any changes made to the slider component change globally

--- web/actions/spaceSceneActions.js
+++ web/actions/spaceSceneActions.js
@@ -1,6 +1,11 @@
 /** Actions types */
 export const UPDATE_SIMULATION_TIME = 'UPDATE_SIMULATION_TIME';
+export const UPDATE_ANIMATION_SPEED = 'UPDATE_ANIMATION_SPEED';
 
 export const updateSimulationTime = (newTime) => dispatch => {
   dispatch({type: UPDATE_SIMULATION_TIME, payload: newTime});
+}
+
+export const updateAnimationSpeed = (newSpeed) => dispatch => {
+  dispatch({type: UPDATE_ANIMATION_SPEED, payload: newSpeed});
 }
