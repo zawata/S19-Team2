cdf0ce20e6247a589ddbc43e3ba68630f5fc3910
Trevor Forrey <trevor4e@gmail.com>
2019-04-25 17:11:05 -0700

    Refactored multi-select controls

--- web/components/toggleControls/TrailControls.js
+++ web/components/toggleControls/TrailControls.js
@@ -3,55 +3,34 @@ import { connect } from 'react-redux';
 import { updateTrailType } from '../../actions/spaceSceneActions';
 import { selectCurrentTrailType } from '../../reducers';
 
+const FULL_TRAIL = 'full';
+const PARTIAL_TRAIL = 'partial';
+const NO_TRAIL = '';
+
 class TrailControls extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showFullTrail: (this.props.currentTrailType === 'full'),
-      showPartialTrail: (this.props.currentTrailType === 'partial'),
-      showNoTrail: (this.props.currentTrailType === '')
+      selectedTrailType: this.props.currentTrailType
     };
     this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick(buttonClicked) {
-    switch(buttonClicked) {
-      case 'fullTrail':
-        this.setState({
-          showFullTrail: true,
-          showPartialTrail: false,
-          showNoTrail: false
-        });
-        this.props.updateTrailType('full');
-        break;
-      case 'partialTrail':
-        this.setState({
-          showPartialTrail: true,
-          showFullTrail: false,
-          showNoTrail: false
-        });
-        this.props.updateTrailType('partial');
-        break;
-      case '':
-        this.setState({
-          showPartialTrail: false,
-          showFullTrail: false,
-          showNoTrail: true
-        });
-        this.props.updateTrailType('');
-    }
+    this.props.updateTrailType(buttonClicked);
+    this.setState({ selectedTrailType: buttonClicked });
   }
 
   render() {
     return(
       <div className="multi-option-toggler">
         <span>Trajectory </span>
-        <button className={'toggleControlsButton' + (this.state.showFullTrail ? ' selected' : '')}
-          onClick={() => {this.handleClick('fullTrail')}}>Full</button>
-        <button className={'toggleControlsButton' + (this.state.showPartialTrail ? ' selected' : '')}
-          onClick={() => {this.handleClick('partialTrail')}}>Partial</button>
-        <button className={'toggleControlsButton' + (this.state.showNoTrail ? ' selected' : '')}
-          onClick={() => {this.handleClick('')}}>None</button>
+        <button className={'toggleControlsButton' + (this.state.selectedTrailType === FULL_TRAIL ? ' selected' : '')}
+          onClick={() => {this.handleClick(FULL_TRAIL)}}>Full</button>
+        <button className={'toggleControlsButton' + (this.state.selectedTrailType === PARTIAL_TRAIL ? ' selected' : '')}
+          onClick={() => {this.handleClick(PARTIAL_TRAIL)}}>Partial</button>
+        <button className={'toggleControlsButton' + (this.state.selectedTrailType === NO_TRAIL ? ' selected' : '')}
+          onClick={() => {this.handleClick(NO_TRAIL)}}>None</button>
       </div>
     )
   }
